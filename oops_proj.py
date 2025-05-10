56efa919a003c57d165648b6c3a8e5741fcd3ed0
Mohit Rajput <mohitraj16@Mac.lan>
2025-05-11 01:24:11 +0400

    covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -13,16 +13,18 @@ class chatbook:
                            2. Press 2 to signin
                            3. Press 3 to write a post 
                            4. Press 4 to message a friend
-                           5. Press any other key to exit""")
+                           5. Press any other key to exit
+                           
+                           """)
 
         if user_input=="1":
             self.signup()
         elif user_input =="2":
             self.signin()
         elif user_input =="3":
-            pass
+            self.my_post()
         elif user_input =="4":
-            pass
+            self.sendmsg()
         else:
             exit()
 
@@ -53,7 +55,31 @@ class chatbook:
 
         print("\n")
         self.menu()        
+    
 
+    def my_post(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            print(f"Following content has been posted ->  {txt} ")
+
+        else:
+            print(f"You need to signin first to post something.....")
+
+        print("\n")
+        self.menu()
+
+
+    def sendmsg(self):
+
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            frnd = input("whom to send the message-> ")
+            print(f" Your message has been sent to {frnd} ")
+        else:
+            print(f"You need to signin first to post something.....")
+
+        print("\n")
+        self.menu()    
 
 
 
--- rough.py
+++ rough.py
@@ -1,11 +1,20 @@
-lst = [1,2,3]
-my_str = 'mlops playlist'
-my_int = 155
+# lst = [1,2,3]
+# my_str = 'mlops playlist'
+# my_int = 155
 
-print(type(my_str),type(lst))
+# print(type(my_str),type(lst))
 
-print(lst)
+# print(lst)
 
-print(lst.clear())
+# print(lst.clear())
 
-print(lst)
+# print(lst)
+
+# print(my_str)
+
+# lst.capitalize()
+
+from oops_proj import chatbook
+
+user1 = chatbook()
+
