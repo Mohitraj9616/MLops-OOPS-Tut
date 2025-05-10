a5b475d04d98361fe04866c77442cb0c192b48cc
Mohit Rajput <mohitraj16@Mac.lan>
2025-05-10 18:36:52 +0400

    class and object basics done

---
+++ oops1.py
@@ -0,0 +1,24 @@
+#initiate a class 
+class employee:
+   # special methods/magic method/ dunder method - constructor 
+    def __init__(self): 
+       print("Started executing attributes/data ")
+       self.id = 123
+       self.salary = 50000
+       self.designation  = "SDE"
+       print("attributes/data have been initiated")
+
+
+    def travel(self, destination):
+        print("This travel medthod was called manually")
+        print(f"Employee is now travelling to {destination}")
+        
+
+    
+
+# create an obj/instance of the class
+sam = employee()
+
+sam.travel("kerela")
+
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,11 @@
+lst = [1,2,3]
+my_str = 'mlops playlist'
+my_int = 155
+
+print(type(my_str),type(lst))
+
+print(lst)
+
+print(lst.clear())
+
+print(lst)
